c77d8eb8ff5032f910d6fa33067d5b6d5b64689e
gabriellaballa <balla_gabriella@yahoo.com>
2023-09-10 22:01:07 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,35 +1,56 @@
-import Vimeo from '@vimeo/player';
+import * as VimeoPlayer from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-// Select the iframe element for the player
-const iframe = document.querySelector('#vimeo-player');
-
-try {
-  // Initialize the Vimeo player
-  const player = new Vimeo.Player(iframe);
-
-  // Add the 'timeupdate' event and use throttle to update the playback time in Local Storage
-  player.on(
-    'timeupdate',
-    throttle(function (data) {
-      const currentTimeInSeconds = data.seconds;
-      localStorage.setItem(
-        'videoplayer-current-time',
-        currentTimeInSeconds.toString()
-      );
-    }, 500)
-  ); // Update once per second (1000 ms)
-
-  // On page load, check if there is saved time in Local Storage and, if so, set the playback time to the saved position
-  window.addEventListener('load', function () {
-    const storedTime = localStorage.getItem('videoplayer-current-time');
-    if (storedTime !== null) {
-      const currentTimeInSeconds = parseFloat(storedTime);
-      player.setCurrentTime(currentTimeInSeconds).catch(error => {
-        console.error('Could not set playback time:', error);
-      });
-    }
-  });
-} catch (error) {
-  console.error('Error initializing Vimeo Player:', error);
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+  const formattedTime = `${minutes}:${
+    remainingSeconds < 10 ? '0' : ''
+  }${remainingSeconds}`;
+  return formattedTime;
 }
+
+function setCurrentTime(videoId, storageKey) {
+  const iframe = document.getElementById(videoId);
+
+  try {
+    // Initialize the Vimeo player
+    const player = new VimeoPlayer.Player(iframe);
+
+    player.on(
+      'timeupdate',
+      throttle(function (data) {
+        const currentTimeInSeconds = data.seconds;
+        const formattedTime = formatTime(currentTimeInSeconds);
+        localStorage.setItem(storageKey, formattedTime);
+      }, 1000) // Update once per second (1000 ms)
+    );
+
+    window.addEventListener('load', function () {
+      const storedTime = localStorage.getItem(storageKey);
+      if (storedTime !== null) {
+        const [minutes, seconds] = storedTime.split(':').map(parseFloat);
+        const currentTimeInSeconds = minutes * 60 + seconds;
+        player.setCurrentTime(currentTimeInSeconds).catch(error => {
+          console.error('Could not set playback time:', error);
+        });
+      }
+    });
+
+    window.addEventListener('beforeunload', () => {
+      player
+        .getCurrentTime()
+        .then(currentTimeInSeconds => {
+          const formattedTime = formatTime(currentTimeInSeconds);
+          localStorage.setItem(storageKey, formattedTime);
+        })
+        .catch(error => {
+          console.error('Could not set playback time:', error);
+        });
+    });
+  } catch (error) {
+    console.error('Error initializing Vimeo Player:', error);
+  }
+}
+
+setCurrentTime('vimeo-player', 'videoplayer-current-time');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,16 +4,13 @@ const feedbackForm = document.querySelector('.feedback-form');
 const emailInput = feedbackForm.querySelector('input[name="email"]');
 const messageInput = feedbackForm.querySelector('textarea[name="message"]');
 
-// Funcție pentru a salva starea în Local Storage
 const saveStateToLocalStorage = throttle(() => {
   const state = {
     email: emailInput.value,
     message: messageInput.value,
   };
   localStorage.setItem('feedback-form-state', JSON.stringify(state));
-}, 500); // Actualizare la fiecare 500 ms
-
-// Funcție pentru a popula câmpurile cu datele din Local Storage la încărcarea paginii
+}, 500); 
 function populateFormFields() {
   const storedState = localStorage.getItem('feedback-form-state');
   if (storedState) {
@@ -23,24 +20,20 @@ function populateFormFields() {
   }
 }
 
-// Încărcați starea la încărcarea paginii
+
 window.addEventListener('load', populateFormFields);
 
-// Ascultați evenimentul "input" pentru a salva starea
 emailInput.addEventListener('input', saveStateToLocalStorage);
 messageInput.addEventListener('input', saveStateToLocalStorage);
 
-// Ascultați evenimentul "submit" pentru formular
 feedbackForm.addEventListener('submit', event => {
   event.preventDefault();
 
-  // Obțineți starea curentă din Local Storage și afișați-o în consolă
   const storedState = localStorage.getItem('feedback-form-state');
   if (storedState) {
     const state = JSON.parse(storedState);
     console.log('Starea curentă a formularului:', state);
   }
 
-  // Ștergeți datele din Local Storage
   localStorage.removeItem('feedback-form-state');
 });
