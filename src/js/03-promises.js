b6d6b65e93213b7368bce8de65eb8b6811e482c9
gabriellaballa <balla_gabriella@yahoo.com>
2023-09-14 20:45:43 +0300

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix';
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      const promiseData = { position, delay };
+      if (shouldResolve) {
+        resolve(promiseData);
+      } else {
+        reject(promiseData);
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const delay = parseInt(formData.get('delay'));
+  const step = parseInt(formData.get('step'));
+  const amount = parseInt(formData.get('amount'));
+
+  for (let i = 1; i <= amount; i++) {
+    const currentDelay = delay + (i - 1) * step;
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
