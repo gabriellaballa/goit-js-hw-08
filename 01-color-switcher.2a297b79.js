e559abe9d54b47a30caafdc511ff0ea7ea989601
gabriellaballa <128022754+gabriellaballa@users.noreply.github.com>
2023-09-14 17:46:13 +0000

    Deploying to gh-pages from @ gabriellaballa/goit-js-hw-08@b6d6b65e93213b7368bce8de65eb8b6811e482c9 🚀

---
+++ 01-color-switcher.2a297b79.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body;let d=null;t.addEventListener("click",(()=>{d||(d=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3),t.disabled=!0)})),e.addEventListener("click",(()=>{d&&(clearInterval(d),d=null,t.disabled=!1)}));
+//# sourceMappingURL=01-color-switcher.2a297b79.js.map
---
+++ 01-color-switcher.4993cf02.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body,a=null;t.addEventListener("click",(function(){a||(a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3),t.disabled=!0)})),e.addEventListener("click",(function(){a&&(clearInterval(a),a=null,t.disabled=!1)}))}();
+//# sourceMappingURL=01-color-switcher.4993cf02.js.map
